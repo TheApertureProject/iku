f5d82c1642814ec9ca8e71f60ddb3446bc57e943
poulpette <apertureproject.me@gmail.com>
2020-04-16 17:28:25 +0200

    Update welcomer.py

--- modules/welcomer.py
+++ modules/welcomer.py
@@ -15,9 +15,9 @@ class Welcomer(Cog):
             aperture_id = self.bot.get_guild(466600971213209600)
             large_message = aperture_id.get_channel(466603496322498561)
             await large_message.send(f""":arrow_right: Bienvenue sur Aperture, {member.mention} !
-            Voici quelques salons à lire avant toute chose : <#539929961512042506> | <#697087325246849084>
-            Des rôles sont aussi disponibles. <#699609186907979797>
-            Profitez-bien et n'hésitez pas à contacter l'équipe en cas de question ou problème :)""")
+Voici quelques salons à lire avant toute chose : <#539929961512042506> | <#697087325246849084>
+Des rôles sont aussi disponibles. <#699609186907979797>
+Profitez-bien et n'hésitez pas à contacter l'équipe en cas de question ou problème :)""")
 
             short_message = aperture_id.get_channel(466600971213209602)
             await short_message.send(f"Bienvenue, {member.mention} :D")
