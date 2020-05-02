4ad5eccfcc15ad0605fe93ccff1b420d2350cdbb
poulpette <apertureproject.me@gmail.com>
2020-05-02 02:09:42 +0200

    Update datacom.py

--- modules/datacom.py
+++ modules/datacom.py
@@ -1,11 +1,24 @@
 import discord
 from discord.ext import commands
 from discord.ext.commands import Cog
+import os
+
+DBU = os.environ["DB_USERNAME"]
+DBP = os.environ["DB_PASSWORD"]
+DBN = os.environ["DB_NAME"]
+DBH = os.environ["DB_HOST"]
+DBP = os.environ["DB_PORT"]
+
+conn = mariadb.connect(user='root', password='default', database='iku_test', host='localhost', port='3306')
+
+botcursor = conn.cursor()
 
 class Datacom(Cog):
 
     def __init__(self, bot):
         self.bot = bot
+
+    
 
 def setup(bot):
     bot.add_cog(Datacom(bot)
