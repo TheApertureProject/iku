10f570b6385dfe24928946455779770766ef9052
poulpette <apertureproject.me@gmail.com>
2020-05-13 23:45:25 +0200

    Create demo.py

---
+++ modules/demo.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+class Demo(Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @bot.command()
+    async def ping(ctx):
+    """Répond 'Ping!'"""
+        await ctx.send("Pong!")
+    
+    @bot.command()
+    async def pong(ctx):
+    """Répond 'Pong!'"""
+        await ctx.send("Ping!")
+
+def setup(bot):
+    bot.add_cog(Demo(bot))
