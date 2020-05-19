87ffbf942f3df39f3332e89fc1dafb5958104c04
poulpette <apertureproject.me@gmail.com>
2020-05-19 10:41:58 +0200

    Update datacom.py

--- modules/datacom.py
+++ modules/datacom.py
@@ -23,10 +23,14 @@ class Datacom(Cog):
     async def register(self, ctx):
         msg = await ctx.send("<a:loading:712211273743597618> | Création de votre profil utilisateur en cours.")
         try:
-            botcursor.execute("INSERT INTO data (userid INT, money INT, Level INT), VALUES ({}, {}, {})".format(ctx.author.id, 1000, 1))
+            botcursor.execute("INSERT INTO data(userid INT, money INT, Level INT), VALUES ({}, {}, {})".format(ctx.author.id, 1000, 1))
         except Exception as e:
             print(e)
-        await msg.edit(content=f":white_check_mark | Votre profil a bien été créé, {ctx.author.mention}. Amusez-vous bien :)")
+        a = f"""":white_check_mark: | Votre profil a bien été créé, {ctx.author.mention}. Voici vos statistiques de départ :
+> :heart_decoration: Rang : 1
+> :credit_card: Crédits : 1000
+Affichez à tout moment ces statistiques avec la commande `profil`. Amusez-vous bien !""""
+        await msg.edit(content=a)
 
 def setup(bot):
     bot.add_cog(Datacom(bot))
