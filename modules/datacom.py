d9236e56cffc2e5670b550cca9135f2367201c2e
poulpette <apertureproject.me@gmail.com>
2020-05-19 12:37:46 +0200

    Update datacom.py

--- modules/datacom.py
+++ modules/datacom.py
@@ -24,10 +24,10 @@ class Datacom(Cog):
         Level = 1
 
         try:
-            await database.execute(f"INSERT INTO data (UserId, Money, Level) VALUES ({}, {}, {})".format(UserId, Money, Level))
+            await database.execute("INSERT INTO data (UserId, Money, Level) VALUES ({}, {}, {})".format(UserId, Money, Level))
         except Exception as e:
             print(e)
-        a = """:white_check_mark: | Votre profil a bien été créé, {ctx.user.mention}. Voici vos statistiques de départ :
+        a = f""":white_check_mark: | Votre profil a bien été créé, {ctx.user.mention}. Voici vos statistiques de départ :
 
 > :heart_decoration: Rang : `{Level}`
 > :credit_card: Crédits : `{Money}`
