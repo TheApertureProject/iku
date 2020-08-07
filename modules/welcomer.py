3d8420da27a9469a794afe5255cd7715217836bf
poulpette <apertureproject.me@gmail.com>
2020-08-07 03:03:25 +0200

    Update welcomer.py

--- modules/welcomer.py
+++ modules/welcomer.py
@@ -11,16 +11,23 @@ class Welcomer(Cog):
     async def on_member_join(self, member):
         if member.guild.id == 466600971213209600:
 
-                        
-            aperture_id = self.bot.get_guild(466600971213209600)
-            large_message = aperture_id.get_channel(466603496322498561)
-            await large_message.send(f""":arrow_right: Bienvenue sur Aperture, {member.mention} !
-Voici quelques salons à lire avant toute chose : <#539929961512042506> | <#697087325246849084>
-Des rôles sont aussi disponibles. <#699609186907979797>
-Profitez-bien et n'hésitez pas à contacter l'équipe en cas de question ou problème :)""")
-
             short_message = aperture_id.get_channel(466600971213209602)
             await short_message.send(f"Bienvenue, {member.mention} :D")
+
+            private_message = """Hey !
+
+Je n'ai pu m'empêcher de remarquer que tu venais de rejoindre **aperture** ! Bienvenue à toi :)
+
+Avant toute chose, je te conseille de te présenter dans le salon <#467021094793117707> en respectant le modèle suivant :
+```**Nom :**
+**Genre :**
+**Année de naissance :**
+**Localisation :**
+**Centres d'intérêts :**
+**Goûts musicaux :**```
+(cela aidera les autres membres à mieux te connaître !)
+
+Bisous et profite bien de notre serveur !"""
 
     @Cog.listener()
     async def on_member_remove(self, member):
